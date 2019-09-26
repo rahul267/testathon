a9330a86d25cd71939af63a93a466030c83e6bda
Neelaveni_Epuri <vimp.neelaveni@gmail.com>
2019-09-26 10:38:00 +0530

    Adding API Steps

--- src/main/java/StoryRunners/RunStories.java
+++ src/main/java/StoryRunners/RunStories.java
@@ -50,7 +50,7 @@ public class RunStories extends JUnitStories {
 
     @Override
     protected List<String> storyPaths() {
-        return new StoryFinder().findPaths(codeLocationFromClass(this.getClass()), "**/TestAutothon/FileUpload_Api.story", "");
+        return new StoryFinder().findPaths(codeLocationFromClass(this.getClass()), "**/TestAutothon/*.story", "");
 
     }
 }
